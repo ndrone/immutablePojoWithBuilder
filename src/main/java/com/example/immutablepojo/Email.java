d72da8bf1617626af4dd77ab6c3c55aacc3335f3
Nicholas Drone <ndrone@gmail.com>
2020-02-16 21:08:30 -0500

    Fixed assertions after switching to Spring

--- src/main/java/com/example/immutablepojo/Email.java
+++ src/main/java/com/example/immutablepojo/Email.java
@@ -7,14 +7,16 @@ import org.springframework.util.Assert;
 
 public class Email {
     private static final String EMAIL_REGEX = "[^@ \\t\\r\\n]+@[^@ \\t\\r\\n]+\\.[^@ \\t\\r\\n]+";
+    private static final String ERROR_MESSAGE = "address must match pattern: " + EMAIL_REGEX;
     private static final Pattern EMAIL_PATTERN = Pattern.compile(EMAIL_REGEX);
 
     private final String address;
     private final String type;
 
     public Email(String address, String type) {
-        Assert.state(EMAIL_PATTERN.matcher(address).matches(),
-                "address must match pattern: " + EMAIL_REGEX);
+        Assert.hasText(address, ERROR_MESSAGE);
+        Assert.isTrue(EMAIL_PATTERN.matcher(address).matches(),
+                ERROR_MESSAGE);
         this.address = address;
         Assert.hasText(type, "type must have text");
         this.type = type;
--- src/main/java/com/example/immutablepojo/Phone.java
+++ src/main/java/com/example/immutablepojo/Phone.java
@@ -6,14 +6,15 @@ import org.springframework.util.Assert;
 
 public class Phone {
     private static final String PHONE_NUMBER_REGEX = "^[+]?[(]?[0-9]{3}[)]?[-\\s.]?[0-9]{3}[-\\s.]?[0-9]{4,6}$";
+    private static final String ERROR_MESSAGE = "number must match pattern: " + PHONE_NUMBER_REGEX;
     private static final Pattern PHONE_PATTERN = Pattern.compile(PHONE_NUMBER_REGEX);
 
     private final String number;
     private final String type;
 
     public Phone(String number, String type) {
-        Assert.state(PHONE_PATTERN.matcher(number).matches(),
-                "number must match pattern: " + PHONE_NUMBER_REGEX);
+        Assert.hasText(number, ERROR_MESSAGE);
+        Assert.isTrue(PHONE_PATTERN.matcher(number).matches(), ERROR_MESSAGE);
         this.number = number;
         Assert.hasText(type, "type must have text");
         this.type = type;
